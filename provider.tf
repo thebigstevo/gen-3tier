c52ace54f18032ba968386849379ca734bde721f
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-12-14 12:05:03 +0000

    declared variables for AK and SK

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
   region = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  
+}
